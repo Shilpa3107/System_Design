b83e45ed6149dbbd714309e46a59fe6f37309791
Shilpa <shilpa.sinha3107@gmail.com>
2023-06-17 17:48:13 +0530

    Changing positions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MethodologyComponent } from './methodology/methodology.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { UmldiagramComponent } from './umldiagram/umldiagram.component';
-import { MethodologyComponent } from './methodology/methodology.component';
+
 
 const routes: Routes = [
   {path: '', component: HomeComponent} ,
