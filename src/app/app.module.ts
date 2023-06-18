79ff91a0fbf4a999753edf1fdb93ea3197949e32
Shilpa <shilpa.sinha3107@gmail.com>
2023-06-19 03:04:56 +0530

    importing tool component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ContactComponent } from './contact/contact.component';
 import { UmldiagramComponent } from './umldiagram/umldiagram.component';
 import { CasestudiesComponent } from './casestudies/casestudies.component';
 import { PracticesComponent } from './practices/practices.component';
+import { ToolComponent } from './tool/tool.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { PracticesComponent } from './practices/practices.component';
     MethodologyComponent,
     CasestudiesComponent,
     PracticesComponent,
+    ToolComponent,
     AboutComponent,
     ContactComponent,
     UmldiagramComponent
