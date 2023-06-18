d38e01c4fc1a24dd99c9c006621d35e508ebf703
Shilpa <shilpa.sinha3107@gmail.com>
2023-06-18 23:22:43 +0530

    Adding casestudies component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MethodologyComponent } from './methodology/methodology.component';
+import { CasestudiesComponent } from './casestudies/casestudies.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { UmldiagramComponent } from './umldiagram/umldiagram.component';
 
 
+
 const routes: Routes = [
   {path: '', component: HomeComponent} ,
   {path: 'methodology', component: MethodologyComponent}, 
+  {path: 'casestudies', component: CasestudiesComponent}, 
   {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent}, 
   {path: 'umldiagram', component: UmldiagramComponent}
