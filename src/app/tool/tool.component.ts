3d14adc2a5d7b813b347cf1b3b3f7c24183d328e
Shilpa <shilpa.sinha3107@gmail.com>
2023-06-26 14:21:13 +0530

    Show the topic's name

--- src/app/tool/tool.component.ts
+++ src/app/tool/tool.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolComponent implements OnInit {
 
-  constructor() { }
+  isCollapsed: boolean = false;
+  topics: Array<any> = []
+
+  toggleTool() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get('assets/jsondata/data1.json').subscribe((data1: any) => {
+      console.log(data1);
+      this.topics = data1.topics;
+      // Use the data1 as needed in your component
+    });
+  }
+
+  toggleSubtopics(topic: any) {
+    topic.showSubtopics = !topic.showSubtopics;
   }
 
 }
