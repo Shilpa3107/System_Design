8b70fe8264f82954b4f8b0e547e29bae211227f7
Shilpa <shilpa.sinha3107@gmail.com>
2023-07-20 14:20:20 +0530

    Addded the sidebar

--- src/app/practices/practices.component.ts
+++ src/app/practices/practices.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PracticesComponent implements OnInit {
 
+  topics : string="practices"
   constructor() { }
 
   ngOnInit(): void {
+    this.topics = "practices";
   }
 
 }
