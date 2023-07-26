edf34233892ae6281f14df8fe5cbd6cfd2e6f510
Shilpa <shilpa.sinha3107@gmail.com>
2023-07-26 14:16:19 +0530

    Showed the content of the sidebar

--- src/app/practices/practices.component.ts
+++ src/app/practices/practices.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-practices',
@@ -6,12 +9,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./practices.component.css']
 })
 export class PracticesComponent implements OnInit {
-
+  topicUrl: string = "";
+  subtopicUrl: string = "";
+  content: string = "";
+  sub: string=" ";
   topics : string="practices"
-  constructor() { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.topics = "practices";
+    this.route.params.subscribe(params => {
+      const subtopic = params.subtopic;
+      console.log("Subtopic received:", subtopic);
+     
+      const [topicUrl, subtopicUrl] = subtopic.split("_");
+      console.log("Parsed topic URL:", topicUrl);
+      console.log("Parsed subtopic URL:", subtopicUrl);
+      this.sub = subtopicUrl;
+      this.http.get('assets/jsondata/practices.json').subscribe((data: any) => {
+        console.log("Data received:", data);
+  
+        if (!data || !data.topics || !Array.isArray(data.topics)) {
+          this.content = "Data format error: Unable to find topics.";
+          return;
+        }
+  
+        const topic = data.topics.find((t: any) => t.url === topicUrl);
+        console.log("Found topic:", topic);
+  
+        if (!topic) {
+          this.content = "Topic not found.";
+          return;
+        }
+  
+        console.log("Topic subtopics:", topic.subtopics);
+  
+        const subtopicObj = topic.subtopics.find((subt: any) => subt.url === subtopicUrl);
+        console.log("Found subtopicObj:", subtopicObj);
+  
+        if (!subtopicObj) {
+          this.content = "Subtopic not found.";
+          return;
+        }
+  
+        this.content = subtopicObj.content;
+      }, (error) => {
+        console.error("Error fetching data:", error);
+        this.content = "Error fetching data. Please try again later.";
+      });
+    });
   }
 
 }
