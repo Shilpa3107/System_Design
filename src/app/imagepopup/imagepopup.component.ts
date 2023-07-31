f7285868e38ec99be5bb76faca7db62ddde618b0
Shilpa <shilpa.sinha3107@gmail.com>
2023-08-01 02:50:24 +0530

    Created a new component

---
+++ src/app/imagepopup/imagepopup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imagepopup',
+  templateUrl: './imagepopup.component.html',
+  styleUrls: ['./imagepopup.component.css']
+})
+export class ImagepopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
