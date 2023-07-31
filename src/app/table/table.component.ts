523a3b3705e51052405429b26f70ece8aaf852b1
Shilpa <shilpa.sinha3107@gmail.com>
2023-07-31 20:49:52 +0530

    Created a dynamic table

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -13,15 +13,23 @@ interface TableData{
 })
 export class TableComponent implements OnInit {
   @Input() data : string = "";
-  tableData: TableData = {name: "", content: []};
+  tableData: TableData[] = [];
 
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
-    this.httpClient.get<TableData>('assets/jsondata/'+this.data+'.json').subscribe((data:any) =>{
-      this.tableData = data;
-      console.log("Fetched data: ",data);
-    });
+    if (this.data) {
+      this.httpClient.get<TableData[]>(`assets/jsondata/${this.data}.json`).subscribe(
+        (data: TableData[]) => {
+          this.tableData = data;
+          console.log("Fetched data: ", data);
+        },
+        (error) => {
+          console.error("Error fetching data:", error);
+          // Add a default value for tableData in case of error
+          this.tableData = [];
+        }
+      );
+    }
   }
-
 }
