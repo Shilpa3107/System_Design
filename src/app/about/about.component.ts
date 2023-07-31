ddf93ecdb8f3ec84d5a330d295f3638f5b08bb7a
Shilpa <shilpa.sinha3107@gmail.com>
2023-08-01 04:07:28 +0530

    Called the imagepopup

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,11 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
 topics: string = "table";
-
-  constructor() {}
+aboutimage: string = "https://i.ytimg.com/vi/7UWBYJjuIL0/maxresdefault.jpg";
+constructor() {}
 
   ngOnInit(): void {
     this.topics = "table";
   }
-
 }
